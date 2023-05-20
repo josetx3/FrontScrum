7608966b10fc29711bc082bb6955c84524bac309
josetx3 <joseleoquintero123@gmail.com>
2023-05-19 23:40:28 -0500

    Primer commit, esta dañado el modulo de area

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './layout/landing-page/landing-page.component';
+import { MainComponent } from './layout/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'app',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      {
+        path: 'area',
+        loadChildren: () => import ('./modules/area/area.module').then(m => m.AreaModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LandingPageComponent } from './layout/landing-page/landing-page.component';
+import { MainComponent } from './layout/main/main.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { FilterAddEmployeesTeamPipe } from './pipes/pages/filter-employees-add-team/filter-add-employees-team.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    SidenavComponent,
+    MainComponent,
+    FooterComponent,
+    FilterAddEmployeesTeamPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ],
+  exports:[
+    FilterAddEmployeesTeamPipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
